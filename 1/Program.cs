673527c1c8ac1a2f7f1875df4e72b53a03a06c40
aFFekT2212 <ksawier2212@gmail.com>
2022-07-21 21:22:57 +0300

    1

---
+++ 1/Program.cs
@@ -0,0 +1,6 @@
+﻿int n = Int32.Parse(Console.ReadLine());
+if (n % 10 == n / 10000 && n % 100 / 10 == n / 1000 % 10)
+    Console.WriteLine("Является палидромом");
+else
+    Console.WriteLine("Не является палиндромом");
+
